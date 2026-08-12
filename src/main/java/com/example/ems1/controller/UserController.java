da3ec1dfb2f4440970dec21436ac7180685dd7f9
p4pratheekj <pratheekj62@gmail.com>
2026-08-12 12:40:06 +0530

    add getUsers endpoint to UserController

--- src/main/java/com/example/ems1/controller/UserController.java
+++ src/main/java/com/example/ems1/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.ems1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,10 @@ public class UserController {
 	@PostMapping("/verify-otp")
 	public String VerifyOtp(@RequestBody VerifyOtpRequest verifyOtpRequest) {
 		return	otpService.VerifyOtp(verifyOtpRequest);
+	}
+	
+	@GetMapping
+	public String getUsers() {
+		return "List of users";
 	}
 }
